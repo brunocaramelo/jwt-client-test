1c669de6e40021c8e82922a116f1d0eddb4e868d
=Bruno Caramelo <=bruno.caramelo5@gmail.com>
2017-11-06 21:25:15 -0200

    auth adicionado ao usuario

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -11,6 +11,7 @@ class UsersController extends Controller
     
     public function __construct()
     {
+        $this->middleware('auth');
     }
 
     public function index( Request $request ) 
